9dd31ed636b5948b2ef267e443b712399d61be53
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-08-17 11:49:15 -0400

    Added prev_position to tables to make UNIQUE constraint optional

--- database.tsx
+++ database.tsx
@@ -1,20 +1,62 @@
-import React, { createContext, useContext, FC, ReactNode } from 'react';
-import * as SQLite from 'expo-sqlite';
+import React, { createContext, useContext, FC, ReactNode } from "react";
+import * as SQLite from "expo-sqlite";
 
 /* Setup database */
 
-const db = SQLite.openDatabase('routines.db');
+const db = SQLite.openDatabase("routines.db");
 
-db.transaction(tx => {
-    // tx.executeSql(
-    //     'DROP TABLE IF EXISTS routines;'
-    // );
+db.transaction(
+  (tx) => {
+    // tx.executeSql(`DROP TABLE IF EXISTS routines;`);
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS routines (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        position INTEGER NOT NULL,
+        prev_position INTEGER DEFAULT NULL,
+        hidden INTEGER DEFAULT 0,
+        UNIQUE (position, prev_position)
+    );`
+    );
+    // Having prev_position in the unique constraint, in addition to having prev_position match position at the start of every transaction, essentially
+    // means that I can enforce `UNIQUE(position)` at any point during the transaction by setting prev_position = position, rather than having it enforced
+    // at all times (which is what happens without prev_position in there). You have to update it manually every time you change positions, which in a way
+    // kind of makes the constraint optional since you have to choose to enforce it, but it's better than having a real-time constraint.
 
     tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS routines (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, position INTEGER NOT NULL UNIQUE, hidden INTEGER DEFAULT 0);'
+      `CREATE TRIGGER IF NOT EXISTS routines_init_prev_position
+      AFTER INSERT ON routines
+      BEGIN
+        UPDATE routines SET prev_position = position WHERE prev_position IS NULL;
+      END;`
     );
-})
 
+    // tx.executeSql(`DROP TABLE IF EXISTS tasks;`);
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        routine_id INTEGER NOT NULL,
+        name TEXT,
+        position INTEGER NOT NULL,
+        prev_position INTEGER DEFAULT NULL,
+        hidden INTEGER DEFAULT 0,
+        FOREIGN KEY (routine_id) REFERENCES routines (id),
+        UNIQUE (routine_id, position, prev_position)
+    );`
+    );
+
+    tx.executeSql(
+      `CREATE TRIGGER IF NOT EXISTS tasks_init_prev_position
+      AFTER INSERT ON tasks
+      BEGIN
+        UPDATE tasks SET prev_position = position WHERE prev_position IS NULL;
+      END;`
+    );
+  },
+  (e) => {
+    console.log(e);
+  }
+);
 
 /* Setup context */
 
@@ -23,21 +65,19 @@ export type DatabaseType = SQLite.WebSQLDatabase;
 const DatabaseContext = createContext<DatabaseType | null>(null);
 
 interface DatabaseProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const DatabaseProvider: FC<DatabaseProviderProps> = ({children}) => {
-    return (
-        <DatabaseContext.Provider value={db}>
-            {children}
-        </DatabaseContext.Provider>
-    )
-}
+export const DatabaseProvider: FC<DatabaseProviderProps> = ({ children }) => {
+  return (
+    <DatabaseContext.Provider value={db}>{children}</DatabaseContext.Provider>
+  );
+};
 
 export const useDatabase = () => {
-    const database = useContext(DatabaseContext);
-    if (!database) {
-        throw new Error('useDatabase must be used within a DatabaseProvider');
-    }
-    return database;
-}
+  const database = useContext(DatabaseContext);
+  if (!database) {
+    throw new Error("useDatabase must be used within a DatabaseProvider");
+  }
+  return database;
+};
